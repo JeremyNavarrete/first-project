13c6ce2b7b207e7eedd6bf21118e651fb9b7895c
jeremy <jeremydenilson03@gmail.com>
2022-11-17 21:58:51 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './Components/main';
+import './Components/style.css'
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/indox.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Epp';
+
+ReactDOM.render(<Epp />, document.getElementById('root'));
